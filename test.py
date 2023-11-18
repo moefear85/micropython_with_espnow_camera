097c4efccbe56edd747ecb9284c50e5886a2fa50
mo FEAR <39313692+moefear85@users.noreply.github.com>
2023-11-18 22:58:39 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 from ili9XXX import ili9341
 import lvgl as lv
 from espidf import HSPI_HOST,VSPI_HOST
-from time import sleep
 
 display=ili9341(double_buffer=False,miso=15, mosi=27, clk=26, cs=13, dc=14, rst=12, backlight=25, backlight_on=1,spihost=HSPI_HOST, mhz=40, factor=8, hybrid=True)
 #lv.init() # display already inits
@@ -12,4 +11,3 @@ btn.align_to(lv.scr_act(), lv.ALIGN.CENTER, 0, 0)
 label = lv.label(btn)
 label.set_text("Hello World!")
 lv.scr_load(scr)
-sleep(1)
